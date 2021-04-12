f31b355c0a8b344473184b70969407f5c03ae3aa
javier-pardinas <javierpb275@gmail.com>
2021-04-12 13:24:33 +0200

    added comments about routes and views. removed component helloworld

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,21 @@
+//This file tells us the roots that we have in our app
+
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '../views/Home.vue' //we components and view cause we are gonna several pages
+//every route must be a page (view)
 
 Vue.use(VueRouter)
 
+//we have different ways to load components. Both ways work perfectly
 const routes = [
+  //this is one way
   {
     path: '/',
     name: 'Home',
     component: Home
   },
+  //this is the other way
   {
     path: '/about',
     name: 'About',
