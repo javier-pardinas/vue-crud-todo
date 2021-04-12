f0da5d367808c3dd9d854c499033715d1a1d3a13
javier-pardinas <javierpb275@gmail.com>
2021-04-12 15:06:15 +0200

    created addTodo() action so that we can add a new todo

--- src/modules/todos/actions.js
+++ src/modules/todos/actions.js
@@ -14,3 +14,25 @@ export async function fetchTodos({commit}) {
         console.log('The query for getting the todos has finished');
     }
 }
+
+
+export async function addTodo({commit, dispatch}, todo) {
+    try {
+        //data: todos from our json object
+        await Vue.axios({
+            method: 'POST',
+            url: '/todos',
+            data: {
+               id: Date.now(),
+               text: todo.text,
+               done: false//by default it is pending
+            }
+        })
+        //dispatch: first argument of an action that gets executed to execute another action
+        dispatch('fetchTodos');//we will show the todos with the new todo that we just added
+    } catch(e) {
+        commit('todosError', e.message);
+    } finally {
+        console.log('The query for creating a todo has finished');
+    }
+}
