2d625417d05b2dbcdaa4ce8065305db0e89b8be1
javier-pardinas <javierpb275@gmail.com>
2021-04-13 09:50:09 +0200

    Created TodoList component

---
+++ src/components/TodoList.vue
@@ -0,0 +1,16 @@
+<template>
+    <div v-if="todos.length">
+    </div>
+    <b-alert show variant="info" v-else>NO TODOS</b-alert>
+</template>
+
+<script>
+export default {
+    props: {
+        todos: {
+            type: Array,
+            required: true
+        }
+    }
+}
+</script>
