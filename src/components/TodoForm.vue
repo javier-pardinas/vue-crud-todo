486315a7f95bf277dd7080e394a29645867d7e2f
javier-pardinas <javierpb275@gmail.com>
2021-04-13 11:27:49 +0200

    created todoform component

---
+++ src/components/TodoForm.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+    </div>
+</template>
+
+<script>
+//validationMixin: group of functionalities that help us work with validation
+import { validationMixin } from 'vuelidate';
+import {required, minLength} from 'vuelidate/lib/validators';
+
+export default {
+    mixins: [validationMixin],
+    props: {
+        todo: {
+            type: Object,
+            required: true
+        },
+        //This is the text of the button
+        todoSubmit: {
+            type: String,
+            default: 'Create Todo'
+        }
+    },
+    //vuelidate offers us this object
+    validations: {
+        todo: {
+            text: {
+                required, 
+                minLength: minLength(4)
+            }
+        }
+    }
+    
+}
+</script>
