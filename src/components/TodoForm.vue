58f72810afafbd1d6d051f0784afad04efdacf6f
javier-pardinas <javierpb275@gmail.com>
2021-04-13 11:59:19 +0200

    finished todoform template.

--- src/components/TodoForm.vue
+++ src/components/TodoForm.vue
@@ -1,6 +1,23 @@
 <template>
-    <div>
-    </div>
+    <b-form @submit.prevent="$emit('processTodo', todo)">
+        <b-form-group id="todo" label="Todo" label-for="todo">
+        <!--:state= manage validation.state of this field. using vuelidate ($v) 
+        we check that the validation does not work properly-->
+            <b-form-input autocomplete="off" id="todo" 
+            v-model="todo.text" :state="$v.todo.text.$invalid" 
+            placeholder="What is your task?" @input="$v.todo.touch">
+            </b-form-input><!--@input=everytime we touch that field this gets executed and this gets updated-->
+            <!--we show the error of the field of this form as long as 
+            v-if="$v.todo.dirty", everytime the input has changed-->
+            <b-form-invalid-feedback id="todoInfo" v-if="$v.todo.dirty">
+            This field is required
+            </b-form-invalid-feedback>
+        </b-form-group>
+        <!--:disabled="$v.todo.$invalid": if the validation did not finished, then we disable-->
+        <b-button type="submit" variant="primary" :disabled="$v.todo.$invalid">
+        {{todoSubmit}}
+        </b-button>
+    </b-form>
 </template>
 
 <script>
