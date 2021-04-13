7705c3216daa775aee4b8c8544381fcaf5543092
javier-pardinas <javierpb275@gmail.com>
2021-04-13 11:12:44 +0200

    created todos component. moved router-view inside container in App.vue so the views displayed are inside this container.

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
   <div id="app">
     <b-container>
-    <b-nav tabs>
-    <b-nav-item exact to="/todos">TODOS LIST</b-nav-item>
-    <b-nav-item exact to="/todos/create">REGISTER TODO</b-nav-item>
-    </b-nav>
+      <b-nav tabs>
+        <b-nav-item exact to="/todos">TODOS LIST</b-nav-item>
+        <b-nav-item exact to="/todos/create">REGISTER TODO</b-nav-item>
+      </b-nav>
+      <router-view/>
     </b-container>
-    <router-view/>
   </div>
 </template>
 
