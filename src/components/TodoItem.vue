4db4994efa703f0794a818b83dc0c954afda0d9e
javier-pardinas <javierpb275@gmail.com>
2021-04-13 10:45:48 +0200

    defined templates of TodoItem and imported TodoItem in TodoList and display them inside this component

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,6 +1,19 @@
 <template>
-    <div>
-    </div>
+    <b-row class="mb-2">
+        <b-col cols="2">{{todo.text}}</b-col>
+        <b-col cols="1">{{todo.done}}</b-col>
+        <b-col>
+            <b-button variant="primary" @click="goToUpdateTodo">
+            EDIT
+            </b-button>
+            <b-button variant="warning" class='ml-2' @click="updateTodoStatus">
+            STATUS
+            </b-button>
+            <b-button variant="danger" class='ml-2' @click="removeTodo">
+            DELETE
+            </b-button>
+        </b-col>
+    </b-row >
 </template>
 
 <script>
@@ -21,18 +34,18 @@ export default {
         }),
         //(module, mutation)
         ...mapMutations('todos', ['setTodo']),
-        goToUpdateTodo(todo) {
-            this.setTodo(todo);
+        goToUpdateTodo() {
+            this.setTodo(this.todo);
             this.$router.push({
                 name: 'todos-update',
-                params: {id: todo.id}
+                params: {id: this.todo.id}
             });
         },
-        updateTodoStatus(todo) {
-            this._updateTodoStatus(todo);
+        updateTodoStatus() {
+            this._updateTodoStatus(this.todo);
         },
-        removeTodo(id) {
-            this._removeTodo(id);
+        removeTodo() {
+            this._removeTodo(this.todo.id);
         }
     }
 }
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,11 +1,17 @@
 <template>
     <div v-if="todos.length">
+    <todo-item v-for="todo in todos" :key="todo.id" :todo="todo"></todo-item>
     </div>
     <b-alert show variant="info" v-else>NO TODOS</b-alert>
 </template>
 
 <script>
+import TodoItem from './TodoItem';
+
 export default {
+    components: {
+        TodoItem
+    },
     props: {
         todos: {
             type: Array,
